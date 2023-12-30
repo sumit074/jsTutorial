2196b455d64b1097a20f76991aa036d8e748523e
sumit074 <127106855+sumit074@users.noreply.github.com>
2023-12-30 06:44:13 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+alert("sumit") // we are using nodejs not browser
+
+// data types
+
+let age = 20   //  integer/number
+let name = "sumit"   // string
+let isMarried = true  // boolean
+let state = null
+
+// number --> 2 to the power 53
+//bigint
+// string --> ""
+// boolean --> true/false
+//null --> standalone value
+//undefined -->
+//symbol --> unique
+
+// object
+
+console.log(typeof "sumit")
+console.log(typeof isMarried)
