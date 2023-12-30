8693211d83a03ae91da1fbf3c5d92cf446b6d716
sumit074 <127106855+sumit074@users.noreply.github.com>
2023-12-30 06:22:01 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accId = 12345
+let accPassword = "abcde"
+var accName = "sumit"
+accCity = "gauriganj"
+accSpan = "5 years"
+let accountState;
+console.table([accId,accPassword,accName,accCity,accountState,accSpan]);
+
+
+//Notes:
+
+// accId = 345 --> not allowed (const value cannot be changed)
+
+/*
+prefer not to use var
+because of issue in block space and functional space
+*/
