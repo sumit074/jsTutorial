9f71491d3e20bc1decb9fe4037e329f19b7fb489
sumit074 <127106855+sumit074@users.noreply.github.com>
2023-12-31 17:04:59 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -26,3 +26,40 @@ function userLogin(username /* = "defaultValue" */){
 const msg = userLogin("sumit")
 console.log(msg)
 //const msg = userLogin() //undefined,just logged in
+
+// rest operator(...) ---> 
+
+function calculateCartPrice(...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,300,800)) // returns an array of numbers
+
+const user = {
+    name: "sumitdas",
+    id: "ab74"
+}
+
+function displayUser(anyobject){
+    return `${anyobject.name} and id is ${anyobject.id}`
+}
+console.log(displayUser(user)) 
+
+//objects can be directly be passed 
+console.log(displayUser(
+    {
+        name:"michael",
+        id:"das"
+    }
+))
+
+const arr = [2,3,4,5,90]
+
+function returnlastValue(getArray){
+    return getArray[getArray.length-1]
+}
+console.log(returnlastValue(arr))
+
+//array can be directly passed 
+console.log(returnlastValue(
+    [3,4,5,6,7,12]
+))
